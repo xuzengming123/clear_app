fd066ec6cbf5b954953a690fcf0fd25fca9e9e62
xuzengming123 <2409090172@qq.com>
2020-08-13 18:50:47 +0800

    往header里面追加数据

--- Until/handle_header.py
+++ Until/handle_header.py
@@ -4,14 +4,11 @@ from Config import header
 
 
 class HandleHeader:
-    def add_to_header(self,key,value):
+    def add_to_header(self,value):
         common_header = header.headers
-        common_header[key] = value
+        common_header.update(value)
         return common_header
 
-    def add_head(self,*key):
-        a = key
-        print(a)
 
 '''
 拿到追加header的数据，是字典格式，
@@ -20,7 +17,5 @@ class HandleHeader:
 
 ahv = HandleHeader()
 if __name__ == '__main__':
-
     headers = {'xxx':'bbb','ccc':'ddd'}
-    # print(ahv.add_to_header('xzc','asd'))
-    print(ahv.add_head(headers))
+    print(ahv.add_to_header(headers))
